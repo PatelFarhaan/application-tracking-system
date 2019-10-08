675fbcec267cc2f043a2f94e6feaf2e7fdddb9dd
Farhaan Patel <patel.farhaaan@gmail.com>
2019-10-08 13:46:34 -0700

    muliple login established

--- project/admin/views.py
+++ project/admin/views.py
@@ -1,14 +1,33 @@
-from flask import render_template, request, Blueprint
+from project.models import Users, Employee
+from werkzeug.security import check_password_hash
+from flask_login import login_required, login_user, logout_user
+from flask import render_template, request, Blueprint, redirect, url_for, session
 
 
 admin_blueprint = Blueprint('admin', __name__, template_folder='templates')
 
 
-@admin_blueprint.route('/admin', methods=['GET', 'POST'])
-def admin_login():
+@admin_blueprint.route('/admin-view', methods=['GET', 'POST'])
+def login():
+    session.clear()
     if request.method == 'POST':
         admin_email = request.form.get('admin_email', None)
         admin_password = request.form.get('admin_password', None)
+
+        if admin_email == None:
+            return render_template('admin.html', warning='Email cannot be empty')
+
+        if admin_password == None:
+            return render_template('admin.html', warning='Password cannot be empty')
+
+        admin_obj = Employee.query.filter_by(email=admin_email).first()
+        if admin_obj == None:
+            return render_template('admin.html', warning='Email address does not exist!!!')
+
+        admin_users_obj = Users.query.filter_by(emp_id=admin_obj.id).first()
+        if admin_obj is not None and check_password_hash(admin_users_obj.hashed_password, admin_password):
+            login_user(admin_obj)
+            return redirect(url_for('admin.create_rc_hr_accounts'))
     return render_template ('admin.html')
 
 
--- project/models.py
+++ project/models.py
@@ -7,7 +7,16 @@ from project import db, login_manager
 ########################################################################################################################
 @login_manager.user_loader
 def user_load(user_id):
-    return Users.query.get(user_id)
+    return Applicant.query.get(user_id)
+########################################################################################################################
+
+
+########################################################################################################################
+###############################################* LOGIN MANAGER *########################################################
+########################################################################################################################
+@login_manager.user_loader
+def user_load(user_id):
+    return Employee.query.get(user_id)
 ########################################################################################################################
 
 
@@ -17,7 +26,7 @@ def user_load(user_id):
 class Department(db.Model):
     __tablename__ = 'department'
 
-    dept_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(64))
 ########################################################################################################################
 
@@ -28,10 +37,10 @@ class Department(db.Model):
 class Applicant(db.Model, UserMixin):
     __tablename__ = 'applicant'
 
-    app_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(64))
     gender = db.Column(db.String(64))
-    status = db.Column(db.String(64))
+    status = db.Column(db.String(64), default="Active")
     birth_date = db.Column(db.DateTime)
     hashed_password = db.Column(db.String(256))
     email = db.Column(db.String(128), unique=True, index=True)
@@ -44,38 +53,37 @@ class Applicant(db.Model, UserMixin):
 class Resume(db.Model):
     __tablename__ = 'resume'
 
-    resume_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     resume = db.Column(db.String(256))
-    app_id = db.Column(db.Integer, db.ForeignKey('applicant.app_id'), nullable=False)
+    app_id = db.Column(db.Integer, db.ForeignKey('applicant.id'), nullable=False)
 ########################################################################################################################
 
 
 ########################################################################################################################
 #################################################* EMPLOYEE *###########################################################
 ########################################################################################################################
-class Employee(db.Model):
+class Employee(db.Model, UserMixin):
     __tablename__ = 'employee'
 
-    emp_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     salary = db.Column(db.Float)
     hire_date = db.Column(db.DateTime)
     name = db.Column(db.String(256), nullable=False)
     email = db.Column(db.String(64), nullable=False)
     status = db.Column(db.String(10), nullable=False)
-    dept_id = db.Column(db.Integer, db.ForeignKey('department.dept_id'), nullable=False)
-    app_id = db.Column(db.Integer, db.ForeignKey('applicant.app_id'), nullable=False)
+    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
 ########################################################################################################################
 
 
 ########################################################################################################################
 #################################################* USERS *##############################################################
 ########################################################################################################################
-class Users(db.Model, UserMixin):
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     type = db.Column(db.String(64), nullable=False)
     hashed_password = db.Column(db.String(256), nullable=False)
-    emp_id = db.Column(db.Integer, db.ForeignKey('employee.emp_id'), nullable=False)
-    dept_id = db.Column(db.Integer, db.ForeignKey('department.dept_id'), nullable=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
 ########################################################################################################################
 
 
@@ -85,7 +93,7 @@ class Users(db.Model, UserMixin):
 class Job(db.Model):
     __tablename__ = 'job'
 
-    job_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     title = db.Column(db.String(64))
     min_salary = db.Column(db.Float)
     max_salary = db.Column(db.Float)
@@ -94,7 +102,7 @@ class Job(db.Model):
     description = db.Column(db.Text)
     status = db.Column(db.String(64), nullable=False)
     visibility = db.Column(db.Boolean, default=False, nullable=False)
-    dept_id = db.Column(db.Integer, db.ForeignKey('department.dept_id'), nullable=False)
+    dept_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
 ########################################################################################################################
 
 
@@ -104,11 +112,11 @@ class Job(db.Model):
 class Application(db.Model):
     __tablename__ = 'application'
 
-    appl_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     appl_date = db.Column(db.DateTime)
     status = db.Column(db.String(64), nullable=False)
-    job_id = db.Column(db.Integer, db.ForeignKey('job.job_id'), nullable=False)
-    app_id = db.Column(db.Integer, db.ForeignKey('applicant.app_id'), nullable=False)
+    job_id = db.Column(db.Integer, db.ForeignKey('job.id'), nullable=False)
+    app_id = db.Column(db.Integer, db.ForeignKey('applicant.id'), nullable=False)
 ########################################################################################################################
 
 
@@ -118,11 +126,11 @@ class Application(db.Model):
 class Interview(db.Model):
     __tablename__ = 'interview'
 
-    int_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     int_date = db.Column(db.DateTime)
     comments = db.Column(db.Text)
     status = db.Column(db.String(64), nullable=False)
-    app_id = db.Column(db.Integer, db.ForeignKey('application.appl_id'), nullable=False)
+    app_id = db.Column(db.Integer, db.ForeignKey('application.id'), nullable=False)
 ########################################################################################################################
 
 
@@ -132,11 +140,11 @@ class Interview(db.Model):
 class Offer(db.Model):
     __tablename__ = 'offer'
 
-    ofr_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     salary = db.Column(db.Float)
     ofr_date = db.Column(db.DateTime)
     start_date = db.Column(db.DateTime)
     status = db.Column(db.String(64), nullable=False)
-    emp_id = db.Column(db.Integer, db.ForeignKey('employee.emp_id'), nullable=False)
-    int_id = db.Column(db.Integer, db.ForeignKey('interview.int_id'), nullable=False)
+    emp_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    int_id = db.Column(db.Integer, db.ForeignKey('interview.id'), nullable=False)
 ########################################################################################################################
