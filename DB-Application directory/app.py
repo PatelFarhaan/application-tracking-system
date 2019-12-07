153c651a4e9100b0d29f65aedece924232833822
Farhaan <patel.farhaaan@gmail.com>
2019-12-06 21:06:03 -0800

    local hp side changes

--- DB-Application directory/app.py
+++ DB-Application directory/app.py
@@ -1,10 +1,5 @@
 from project import app
-import logging, logging.config, yaml
+
 
 if __name__ == "__main__":
-    logging.config.dictConfig(yaml.load(open('logging.conf')))
-    logfile = logging.getLogger('file')
-    logconsole = logging.getLogger('console')
-    logfile.debug("Debug FILE")
-    logconsole.debug("Debug CONSOLE")
     app.run(debug=True)
